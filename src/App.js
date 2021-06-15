e3f3203e819a39c16e51b020928449c93da2a8c1
jonathanlucas1489 <jonathan.morais@ccc.ufcg.edu.br>
2021-06-15 07:11:04 -0300

    Almost nothing

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import { Route, Switch } from 'react-router-dom'
 import { useMoralis } from "react-moralis";
-import { Container } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 
 function App() {
   const { isAuthenticated, isAuthenticating } = useMoralis()
   return (
-    <Container>
-      {
-        isAuthenticated ? (
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/perfil" exact>
-            <Profile />
-          </Route>
-        </Switch>) : (
-        <div> cuu</div>
-        )
-      }
-
-    </Container>
+    <Flex direction='column'>
+      <Box>
+        {
+          isAuthenticated ? (
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/perfil" exact>
+                <Profile />
+              </Route>
+            </Switch>) : (
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            </Switch>
+          )
+        }
+      </Box>
+    </Flex>
   )
 }
 export default App;
